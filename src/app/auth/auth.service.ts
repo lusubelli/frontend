405de1c7b3a8088380dbbeb792ac2c2b8945439c
Lionel Usubelli <lionel.usubelli@ext.bpifrance.fr>
2020-03-26 13:57:28 +0100

    [TECH] totototo

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -13,7 +13,7 @@ export class AuthService {
   }
 
   signin(login: any) {
-    return this.http.post<AccessToken>(`${environment.apiUrl}/v1/signin`, login);
+    return this.http.post<AccessToken>(`${environment.apiUrl}/api/v1/signin`, login);
   }
 
   isAuthenticated(): boolean {
@@ -24,12 +24,12 @@ export class AuthService {
   account() {
     const token = localStorage.getItem('xsrfToken');
     const headers = new HttpHeaders({'x-xsrf-token':token});
-    return this.http.get(`${environment.apiUrl}/v1/secured/account`, { headers, withCredentials: true });
+    return this.http.get(`${environment.apiUrl}/api/v1/secured/account`, { headers, withCredentials: true });
   }
 
   signout() {
     const token = localStorage.getItem('xsrfToken');
     const headers = new HttpHeaders({'x-xsrf-token':token});
-    return this.http.get(`${environment.apiUrl}/v1/secured/signout`, { headers, withCredentials: true });
+    return this.http.get(`${environment.apiUrl}/api/v1/secured/signout`, { headers, withCredentials: true });
   }
 }
